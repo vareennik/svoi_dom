b4570d963d8ac7705c4cd6a90d10337fecfd7fb4
vareennik <varvara.frolova@gmail.com>
2016-06-08 23:43:13 +0300

    Gallery, content

--- contact.php
+++ contact.php
@@ -44,7 +44,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "addyouremail@yoursite.com";
+$address = "varvara.frolova@gmail.com";
 
 
 // Configuration option.
